7159ad523befc76c9cb3650128e6451fe2fc3882
anglikanskieStrony <karol.pasiut@gmail.com>
2015-07-19 16:30:01 +0200

    tmp

--- app/views/kategoria-admin-view.php
+++ app/views/kategoria-admin-view.php
@@ -10,15 +10,19 @@ function areUSure()
 	}
 }
  </script>
-<button type="submit" class="btn_admin" style="left:55%;" onClick="areUSure();">Usuń</button>
+<button type="submit" class="btn_admin" style="left:51%;" onClick="areUSure();">Usuń</button>
  <form method="post" action="/admin/menu/kategoria/index.php?categoryid=<?php echo($this->getPageData()->getId()); ?>&amp;action=save">
 	<input type="submit" class="btn_admin" value="zapisz">
 	
 	<div id="content">	
 		 <h1>Edycja kategorii</h1>
-		 <div>nazwa: <input type="text" name="name" id="name" value="<?php echo($this->getPageData()->getName()); ?>"></div><br>
-    	<textarea id="elm1" name="content"><?php echo($this->getPageData()->getContent()); ?></textarea>
-	</div>
+		 <div style="width: 100%;height:120px;">
+		 	<div style="float:left;position:relative;width:20%;height:100%;"><p style="font-size: 1vmax;">nazwa:</p></div>
+		 	<div style="float:left; width:79%;height:100%;"><input type="text" class="text_box" style="margin:0;width:90%;font-size:1vmax" name="name" id="name" value="<?php echo($this->getPageData()->getName()); ?>"></div>
+		 	<br><br>
+		 </div>
+ <textarea id="elm1" name="content"><?php echo($this->getPageData()->getContent()); ?></textarea>
+</div>
 </form>
 
     
