ee82fcb9d02fe609862602352cf7d8f69c03d3bf
anglikanskieStrony <karol.pasiut@gmail.com>
2015-07-20 18:53:01 +0200

    rtm

--- app/views/header-admin-view.php
+++ app/views/header-admin-view.php
@@ -22,17 +22,29 @@
         	 plugins: [
 					 "autoresize",
         	         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+        	      
         	         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
         	         "save table contextmenu directionality emoticons template paste textcolor"
         	   ],
-        	   content_css: "css/content.css",
-        	   toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons", 
-        	   style_formats: [
+        	   content_css: "css/content.css,http://fonts.googleapis.com/css?family=Shojumaru&subset=latin",
+        	   toolbar: "insertfile undo redo | styleselect | sizeselect | fontselect | fontsize | fontsizeselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons", 
+        	   fontsize_formats: "0.1vmax 0.2vmax 0.3vmax 0.4vmax 0.5vmax 0.6vmax 0.7vmax 0.8vmax 0.9vmax 1vmax 1.1vmax 1.2vmax 1.3vmax 1.4vmax 1.5vmax 1.6vmax 1.7vmax 1.8vmax 1.9vmax 2vmax 2.5vmax 3vmax",
+        	   font_formats :	   "Arial=arial,helvetica,sans-serif;"+
+					               "Georgia=georgia,palatino;"+
+					               "Helvetica=helvetica;"+
+					               "Impact=impact,chicago;"+
+					               "Symbol=symbol;"+
+					               "Shojumaru=Shojumaru,sans-serif;"+ 
+					               "Tahoma=tahoma,arial,helvetica,sans-serif;"+
+					               "Terminal=terminal,monaco;"+
+					               "Verdana=verdana,geneva;"+
+					               "Webdings=webdings",
+				image_advtab: true,
+				style_formats: [
         	        {title: 'Tytuł', block: 'h1', styles: {color: '#FFFF00','font-family': 'Shojumaru','font-size':'2vmax','line-height':'0.5vmax', 'text-shadow': '2px 2px #500000'}},
         	        {title: 'Podtytuł', inline: 'span', styles: {color: '#ff0000','font-family': 'Shojumaru','font-size':'1.5vmax','line-height':'0.5vmax', 'text-shadow': '2px 2px #500000'}},
         	        {title: 'Treść', inline: 'span', classes: 'example1', styles: {color:'#FFFF00','font-family': 'Shojumaru','text-shadow': '1px 1px #000000','font-size':'1.2vmax'}},
         	        {title: 'Tłumaczenie', inline: 'span', styles: {color: '#ffffff','font-family': 'Shojumaru','text-shadow': '1px 1px #000000','font-size':'0.7vmax'}},
-        	        {title: 'Table styles'},
         	    ]
             	  
             	 }); 
--- app/views/uzytkownik-admin-view.php
+++ app/views/uzytkownik-admin-view.php
@@ -19,14 +19,16 @@
 <h1>Utwórz pierwszego użytkownika</h1>
 
 <form id="form" action="/admin/user/?action=add" method="post" onsubmit="validate(); return false;">
+	<input type="submit" class="btn_admin" style="width:16.5%; right:2%;background: rgb(144,0,0);" value="utwórz">
+	<br>
 	<p>Login:</p>
 	<input type="text" name="login" class="text_box" id="login" required="true">
 	<p>Hasło:</p>
 	<input type="password" name="password" class="text_box" id="password" required="true">
 	<p>Powtórz hasło:</p>
 	<input type="password" name="password-repeat" class="text_box" id="password-repeat" required="true">
-	<input type="submit" class="btn" style="width:30%;margin-left:35%;margin-right:35%" value="utwórz">
 </form>
+
 
 </div>
 <?php include($_SERVER['DOCUMENT_ROOT']."/app/views/footer-view.php"); ?>
