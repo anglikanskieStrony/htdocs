c249252cf36bd64d05e96cd262d1da499998762f
anglikanskieStrony <dren311@gmail.com>
2015-08-17 22:40:43 +0200

    products

--- index.php
+++ index.php
@@ -133,12 +133,14 @@
 			    			data--100-bottom="opacity: 1;" 
 			    			data-anchor-target="#slide-5"
 			    		>
+							<h1>Kurwa mac</h1>
+							<p>Gatka szmatka yabess szmery bajery lorem ipsum bla bla bla na pohybel skurwysynam, byli zdrajcami i ubekami, teraz się zowią patryjotami, my jednak wiemy, prawda jest inna.</p>
 			    		</div>
 			    	</div>
 				</div>
 			</section>
 	</section>
-	<section id="slide-6" class="homeSlide">
+	<section id="slide-6">
 		<div class="bcg"
 			 data-center="background-position: 50% 40%;"
 			 data-top-bottom="background-position: 50% 40%;"
@@ -168,51 +170,48 @@
 			</div>
 		</div>
 	</section>
-			
-		    <!-- 
-			<section id="slide-3" class="homeSlide">
-				<div class="bcg" 
-					data-center="background-position: 50% 0px;" 
-					data-top-bottom="background-position: 50% -100px;" 
-					data-bottom-top="background-position: 50% 100px;" 
-					data-anchor-target="#slide-3"
-				>
-			    	<div class="hsContainer">
-						<div class="hsContent" 
-							data--50-bottom="opacity: 0;" 
-							data--200-bottom="opacity: 1;" 
-							data-center="opacity: 1" 
-							data-200-top="opacity: 0" 
-							data-anchor-target="#slide-3 h2"
-						>
-				    		<h2>Kurwa Mac_3</h2>
-			    		</div>
-			    	</div>
-			    	
-			    </div>
-			</section>
-			
-			<section id="slide-4" class="homeSlide">
-				<div class="bcg" 
-					data-center="background-position: 50% 0px;" 
-					data-top-bottom="background-position: 50% -100px;" 
-					data-bottom-top="background-position: 50% 100px;" 
-					data-anchor-target="#slide-4"
-				>
-			    	<div class="hsContainer">
-						<div class="hsContent" 
-							data-bottom-top="opacity: 0" 
-							data-25p-top="opacity: 0" 
-							data-top="opacity: 1"  
-							data-anchor-target="#slide-4"
-						>
-				    		<h2>Kurwa Mac_4</h2>
-			    		</div>
-			    	</div>
-			    	
-			    </div>
-			</section>
-			 -->
+	<section id="slide-7">
+		<div class="bcg"
+			 data-center="background-position: 50% 40%;"
+			 data-top-bottom="background-position: 50% 40%;"
+			 data-bottom-top="background-position: 50% 10%;"
+			 data-anchor-target="#slide-5"
+			>
+			<div class="hsContainer">
+				<h1>Products</h1>
+			</div>
+		</div>
+	</section>
+	<section id="slide-6">
+		<div class="bcg"
+			 data-center="background-position: 50% 40%;"
+			 data-top-bottom="background-position: 50% 40%;"
+			 data-bottom-top="background-position: 50% 10%;"
+			 data-anchor-target="#slide-5"
+			>
+			<div class="hsContainer">
+				<div class="slide-6-column">
+					<h2>Kurwa mac</h2>
+					<p>
+						Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+					</p>
+				</div>
+				<div class="slide-6-column">
+					<h2>Kurwa mac</h2>
+					<p>
+						Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+					</p>
+				</div>
+
+				<div class="slide-6-column">
+					<h2>Kurwa mac</h2>
+					<p>
+						Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
 
 </main>
 
